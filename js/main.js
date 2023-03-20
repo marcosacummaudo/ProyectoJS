b67009a7b204492cecb56e79f08de293a1dae9ab
Marcos Cummaudo <marcosacummaudo@gmail.com>
2023-03-20 10:43:55 -0300

    Con array de sabores

--- js/main.js
+++ js/main.js
@@ -6,58 +6,55 @@ function calcularVuelto(pago, importe) {
             "¡Muchas gracias por su compra!")
 }
 
-function revisarPago(precio) {
+function revisarPago(opcionElegida) {
     let pago
+    let precioAPagar = 0
+    opcionElegida.forEach(el => {
+        precioAPagar = precioAPagar + el.precio
+    });
     do {
         do {
-            pago = parseFloat(prompt("El importe a pagar es $" + precio + ", ¿con cuanto abonara?\n" +
-                                        "Debe ingresar un número igual o mayor que $" + precio))
+            pago = parseFloat(prompt("El importe a pagar es $" + precioAPagar + ", ¿con cuanto abonara?\n" +
+                                    "Debe ingresar un número igual o mayor que $" + precioAPagar))
         } while (isNaN(pago))
-    } while(pago<precio)
-    calcularVuelto(pago,precio)
+    } while(pago<precioAPagar)
+    calcularVuelto(pago,precioAPagar)
 }
 
 function saludar() {
-    let precio
-    do {
-        opcion = prompt(mensajeInicial)
-        if(opcion === null) {
-            alert("Acaba de cancelar el inicio de su compra.")
-            break
-        }
-        if(opcion!=='1' && opcion!=='2' && opcion!=='3' && opcion!=='4' && opcion!=='5') {
-            alert("Debe ingresar un número del 1 al 5.")
-        }
-    } while (opcion!=='1' && opcion!=='2' && opcion!=='3' && opcion!=='4' && opcion!=='5')
-    switch(opcion) {
-        case "1":
-            precio = 2000
-            break
-        case "2":
-            precio = 1000
-            break
-        case "3":
-            precio = 500
-            break
-        case "4":
-            precio = 400
-            break
-        case "5":
-            precio = 250
-            break
-        default:
-            precio = 0
-            break
-    }
-    if (precio !== 0) {
-        revisarPago(precio)
+    // let precio
+    let respuesta1 = "False"
+        do {
+            do {
+                opcion = prompt(mensajeInicial)
+                if(opcion === null) {
+                    alert("Acaba de cancelar el inicio de su compra.")
+                    break
+                }
+                if(opcion!=='1' && opcion!=='2' && opcion!=='3' && opcion!=='4' && opcion!=='5') {
+                    alert("Debe ingresar un número del 1 al 5.")
+                }
+            } while (opcion!=='1' && opcion!=='2' && opcion!=='3' && opcion!=='4' && opcion!=='5')
+            if (opcion !== null) {
+                opcionNumero = parseInt(opcion)
+                opcionElegida = tamHelados.find((el) => el.codigo === opcionNumero)
+                compra.push(opcionElegida)
+            respuesta1 = confirm("¿Desea otro helado?")
+            }
+            else{
+                break
+            }
+        }while (respuesta1)
+    if (opcion !== null) {
+        revisarPago(compra)
     }
 }
 
 function ejecutar() {
-    //debugger
+    debugger
     let respuesta = "False"
     do {
+        compra.splice(0, compra.length)
         saludar()
         respuesta = confirm("¿Desea continuar comprando?")
     } while (respuesta)
