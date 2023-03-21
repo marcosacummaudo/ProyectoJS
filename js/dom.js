347aeb670ad17bd8a5992ee947261eb7453e2ae6
Marcos Cummaudo <marcosacummaudo@gmail.com>
2023-03-21 09:56:05 -0300

    Con CSS

--- js/dom.js
+++ js/dom.js
@@ -2,17 +2,17 @@
 let lista = document.getElementById("listaPrecio")
 
 tamHelados.forEach(el => {
-    lista.innerHTML += `<li>${el.codigo} - ${el.nombre}: $${el.precio}</li>`
+    lista.innerHTML += `<li class="div__ul__li">${el.codigo} - ${el.nombre}: $${el.precio}</li>`
 })
 
 let listaSab = document.getElementById("listaSabores")
 
 sabores.forEach(el => {
-    listaSab.innerHTML += `<li>${el.codigo} - ${el.nombre}: ${el.descripcion}</li>`
+    listaSab.innerHTML += `<li class="div__ul__li">${el.codigo} - ${el.nombre}: ${el.descripcion}</li>`
 })
 
 let compraReal = document.getElementById("compraRealizada")
 
 compra.forEach(el => {
-    compraReal.innerHTML += `<li>${el.codigo} - ${el.nombre}: $${el.precio}</li>`
+    compraReal.innerHTML += `<li class="div__ul__li">${el.codigo} - ${el.nombre}: $${el.precio}</li>`
 })
